d6d725ec16f8b80ebad88e061ca35d5c9526ab4d
rishonkumar <rishon.kumar.rahi@gmail.com>
2025-12-24 18:57:56 +0530

    Fixing the bug Hashing passwords

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,5 @@
 import { User } from "../db/models/user.model"
-import {genSalt} from "bcrypt"
-import {hash} from "node:crypto";
+import { genSalt, hash } from "bcrypt"
 import jwt from "jsonwebtoken";
 class UserService {
     public findUserByEmail = async (email: string): Promise<User | null> => {
@@ -8,15 +7,19 @@ class UserService {
         return user
     }
 
-    public createUser = async(email: string, password: string): Promise<User> => {
+    public createUser = async (email: string, password: string): Promise<User> => {
         const salt = await genSalt()
-        const hashedPassword = await hash(password,salt)
-        const verificationToken =jwt.sign({email},"verify_secret")
+        // console.log("Salt", salt)
+        const hashedPassword = await hash(password, salt)
+        // console.log("Hashed Password", hashedPassword)
+        const verificationToken = jwt.sign({ email }, "verify_secret")
+        // console.log("Verification Token", verificationToken)
         const user = await User.create({
             email: email,
             password: hashedPassword,
             verificationToken: verificationToken
         })
+        console.log("User", user)
         return user
     }
 }
